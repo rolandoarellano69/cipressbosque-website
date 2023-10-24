b4b446b3c777f81f641d3097cdace108c8e60cad
Rolando Arellano <rolandoarellano9869@hotmail.com>
2023-10-24 10:46:58 -0700

    Implementacion SEO

--- src/components/Mapa.jsx
+++ src/components/Mapa.jsx
@@ -3,7 +3,7 @@ import mapapeek from '../assets/img/mapa FINAL CIPRES-01.jpg'
 const Mapa = () => {
   return (
   <figure id='ubi' className=''>
-    <figcaption className=" font-semibold text-center bg-gray-300 px-12 py-3 text-secondary mt-8 mb-8 md:p-4 sm:p-2 md:text-2xl lg:text-4xl xl:text-5xl">Ubicación</figcaption>
+    <figcaption className=" font-semibold text-center bg-white px-12 py-3 text-secondary mt-8 mb-8 md:p-4 sm:p-2 md:text-2xl lg:text-4xl xl:text-5xl">Ubicación</figcaption>
     <img title='mapa' src={mapapeek} alt="MAPA" />
   </figure>
   )
